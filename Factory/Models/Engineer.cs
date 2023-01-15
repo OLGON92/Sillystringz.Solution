c4af143969d59e49e8220a12bbf4690b82874f95
OLGON92 <ogon318@gmail.com>
2023-01-14 16:37:40 -0800

    add a required feature

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System;
 
 namespace Factory.Models
 {
   public class Engineer
   {
+    [Required(ErrorMessage = "Engineer's name required!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Specialty required!")]
     public string Specialty { get; set; }
     public int EngineerId { get; set; }
 
