052fd186d04223133080b00b74e9d14beebdbc7b
OLGON92 <ogon318@gmail.com>
2023-01-14 16:21:42 -0800

    update code

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public int EngineerId { get; set; }
+
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
