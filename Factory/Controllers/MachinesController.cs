9302bf9c7826c0aa54e2fc6c63649e35ef672ff2
OLGON92 <ogon318@gmail.com>
2023-01-14 22:07:43 -0800

    add details to MachinesController

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -44,6 +44,15 @@ namespace Factory.Controllers
       }
     }
 
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+              .Include(machine => machine.JoinEntities)
+              .ThenInclude(join => join.Engineer)
+              .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);         
+    }
+
     
   }
 }
