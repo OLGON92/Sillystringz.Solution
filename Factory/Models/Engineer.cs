887e2a840a78c027e7d0a9485f3b5252e20010a3
OLGON92 <ogon318@gmail.com>
2023-01-14 16:58:27 -0800

    fix a placement

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,11 +6,12 @@ namespace Factory.Models
 {
   public class Engineer
   {
+    public int EngineerId { get; set; }
     [Required(ErrorMessage = "Engineer's name required!")]
     public string Name { get; set; }
     [Required(ErrorMessage = "Specialty required!")]
     public string Specialty { get; set; }
-    public int EngineerId { get; set; }
+    
 
     public List<EngineerMachine> JoinEntities { get; }
   }
